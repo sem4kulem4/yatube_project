cb753db2f81580e92c85be8c17f08fcb031835ea
Семен Протасов <sem4kulem4@yandex.ru>
2022-01-26 16:26:00 +0300

    hw2_1. Groups and posts

---
+++ yatube/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('group_list/',views.group_list,name='list' ),
+    path('group/<slug:slug>/', views.group_posts, name = 'group_list')
+    ]
